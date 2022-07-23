ef85f88800a1226414aa82edf1e8f5081d57d36d
mulw <107396543+mulw@users.noreply.github.com>
2022-07-23 09:41:07 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+first_number = float(input("Enter your first input:"))
+
+second_number = float(input("Enter your second input "))
+
+Addition = first_number+second_number
+
+difference = first_number-second_number
+
+multiplication = first_number*second_number
+
+division = (first_number/second_number)
+
+module = (first_number%second_number)
+
+print("The sum is: "  + str(Addition))
+
+print('The resulting difference is: '+str(difference))
+
+print("The division is equal to: " + str(division))
+
+print("The resulting multiplication is: " + str(multiplication))
+
+print("the Remainder after the resulting division is: "+str(module))
